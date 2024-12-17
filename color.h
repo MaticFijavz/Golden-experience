68537f43bfc9c26e0e5644e8162315252f09525c
Matic Fijavz <matic.fijavz.19625@dijak.sc-celje.si>
2024-12-17 16:22:14 +0000

    added a ray_color  function for ray tracing (meh sorta, technically, yes) Added a camera and a viewport and tuned the aglgorithm so that it outputs a y-axis gradient.

--- color.h
+++ color.h
@@ -1,26 +1,24 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-//VECTORS JEAHHHH
 #include "vec3.h"
 
-//std
-#include <fstream>
-#include <filesystem>
+#include <iostream>
 
 using color = vec3;
 
-void write_color(std::ofstream& img , const color& pixel_color) {
+void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
 
-    //translate color to 0-255 [0-1] to [0-255]
+    // Translate the [0,1] component values to the byte range [0,255].
     int rbyte = int(255.999 * r);
     int gbyte = int(255.999 * g);
     int bbyte = int(255.999 * b);
 
-    //write the pixel colors
-    img << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+    // Write out the pixel color components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
 }
+
 #endif
