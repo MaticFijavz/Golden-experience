09c94f0b48faa0e2f723e0bba2746ce2ad8b2f24
Matic Fijavz <matic.fijavz.19625@dijak.sc-celje.si>
2024-12-22 10:43:38 +0000

    added front face and back face normal calculation

--- sphere.h
+++ sphere.h
@@ -8,7 +8,7 @@ class sphere : public hittable {
     public:
         sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
 
-        bool hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec) const override {
+    bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const {
             vec3 oc = center -r.origin();
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
@@ -31,7 +31,8 @@ class sphere : public hittable {
 
             rec.t = root;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
 
             return true;
         }
