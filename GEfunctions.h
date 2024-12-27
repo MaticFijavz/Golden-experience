e76cf632520772a38b38d1710ff7d8a02551d33a
Matic Fijavz <matic.fijavz.19625@dijak.sc-celje.si>
2024-12-27 14:58:11 +0000

    Added Common Constants and Utility Functions

---
+++ GEfunctions.h
@@ -0,0 +1,31 @@
+#ifndef GEFUNCTIONS_H
+#define GEFUNCTIONS_H
+
+#include <cmath>
+#include <iostream>
+#include <limits>
+#include <memory>
+
+//C++ std usings
+
+using std::make_shared;
+using std::shared_ptr;
+
+//Constants
+
+const double infinity = std::numeric_limits<double>::infinity();
+const double pi = 3.1415926535897932385;
+
+//Utility functions
+
+inline double degrees_to_radians(double degrees) {
+    return degrees * pi / 180.0;
+}
+
+//common headers
+
+#include "vec3.h"
+#include "ray.h"
+#include "color.h"
+
+#endif //GEFUNCTIONS_H
