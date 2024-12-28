a2994573d9b997ec2185c8525fabbce93329b607
Matic Fijavz <matic.fijavz.19625@dijak.sc-celje.si>
2024-12-28 16:14:30 +0000

    added MSAA but horridly innefficent

--- GEfunctions.h
+++ GEfunctions.h
@@ -1,15 +1,16 @@
 #ifndef GEFUNCTIONS_H
 #define GEFUNCTIONS_H
 
-#include <cmath>
-#include <iostream>
+#include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 //C++ std usings
 
 using std::make_shared;
 using std::shared_ptr;
+using std::rand;
 
 //Constants
 
@@ -22,6 +23,23 @@ inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
 }
 
+//your mother
+inline double random_double() {
+    //returns a random real in [0,1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    //returns a random real in [min,max)
+    return min + (max-min)*random_double();
+}
+/*
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);                                             what?
+}
+*/
 //common headers
 
 #include "vec3.h"
