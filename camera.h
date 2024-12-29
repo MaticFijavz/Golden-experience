4579d89621c42211583f5b1ed1fc76317e0851d2
Matic Fijavz <matic.fijavz.19625@dijak.sc-celje.si>
2024-12-29 16:42:32 +0000

    limited the amount of child rays with max depth on diffuse sphere

--- camera.h
+++ camera.h
@@ -6,9 +6,10 @@
 
 class camera {
     public:
-    double aspect_ratio     {1.0};  //Ratio of image width over height
-    int    image_width      {400};  //Rendered image width in pixel count
-    int    samples_per_pixel    {1000};  //Count of random samples per pixel
+    double aspect_ratio         {1.0};  //Ratio of image width over height
+    int    image_width          {400};  //Rendered image width in pixel count
+    int    samples_per_pixel    {72};   //Count of random samples per pixel
+    int    max_depth            {10};   //maximum ray bounces in scene
 
     void render(const hittable& world) {
         initialize();
@@ -40,7 +41,7 @@ class camera {
                 color pixel_color(0, 0, 0);
                 for(int sample = 0; sample < samples_per_pixel; sample++) {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r,max_depth, world);
                 }
                 write_color(img, pixel_samples_scale * pixel_color);
             }
@@ -84,12 +85,15 @@ private:
             center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
-    color ray_color(const ray& r, const hittable& world) const {
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        if(depth <= 0) {
+            return color(0,0,0);
+        }
         hit_record rec;
 
         if (world.hit(r, interval(0, infinity), rec)) {
             vec3 direction = random_on_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
